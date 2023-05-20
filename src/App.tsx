6f85cbdf242cda0b039762da2d08a9dd3bd6c310
random776 <kazuki.tdn@gmail.com>
2023-05-20 22:54:38 +0900

    iniT

--- src/App.tsx
+++ src/App.tsx
@@ -10,45 +10,48 @@ function App() {
   // n進変換を行う
   const nmin = 2;
   const nmax = 16;
-  const [before, setBefore] = useState(
-    Math.floor(Math.random() * (nmax + 1 - nmin)) + nmin
-  );
-  const [after, setAfter] = useState(
-    Math.floor(Math.random() * (nmax + 1 - nmin)) + nmin
-  );
+  let a = Math.floor(Math.random() * (nmax + 1 - nmin)) + nmin;
+  let b = Math.floor(Math.random() * (nmax + 1 - nmin)) + nmin;
+  while (a === b) {
+    a = Math.floor(Math.random() * (nmax + 1 - nmin)) + nmin;
+    b = Math.floor(Math.random() * (nmax + 1 - nmin)) + nmin;
+  }
+  const [before, setBefore] = useState(a);
+  const [after, setAfter] = useState(b);
   // 見えるか見えないか
   const [visible, setVisible] = useState(false);
   return (
     <>
-    <h2>フラッシュn進法ニキ</h2>
+      <h2>フラッシュn進法ニキ</h2>
       <p>
-      <MathComponent
+        <MathComponent
           display={false}
           tex={String.raw`${number.toString(before)}_{(${before})}`}
-        ></MathComponent> を、{after}進表示しましょう。
+        ></MathComponent>{" "}
+        を、{after}進表示しましょう。
       </p>
       {visible === false && (
-          <button
-            type="button"
-            key={"answer"}
-            onClick={() => {
-              setVisible(true);
-            }}
-          >
-            答え合わせ
-          </button>
-        )}
-        {visible === true && (
-          <button
-            type="button"
-            key={"answer"}
-            onClick={() => {
-              setVisible(false);
-            }}
-          >
-            答え合わせ
-          </button>
-        )}
+        <button
+          type="button"
+          key={"answer"}
+          onClick={() => {
+            setVisible(true);
+          }}
+        >
+          答え合わせ
+        </button>
+      )}
+      {visible === true && (
+        <button
+          type="button"
+          key={"answer"}
+          onClick={() => {
+            setVisible(false);
+          }}
+        >
+          答え合わせ
+        </button>
+      )}
       <p style={{ visibility: visible ? "visible" : "hidden" }}>
         <MathComponent
           display={false}
@@ -59,9 +62,15 @@ function App() {
         type="button"
         key={"button"}
         onClick={() => {
+          let a = Math.floor(Math.random() * (nmax + 1 - nmin)) + nmin;
+          let b = Math.floor(Math.random() * (nmax + 1 - nmin)) + nmin;
+          while (a === b) {
+            a = Math.floor(Math.random() * (nmax + 1 - nmin)) + nmin;
+            b = Math.floor(Math.random() * (nmax + 1 - nmin)) + nmin;
+          }
+          setAfter(a);
+          setBefore(b);
           setNumber(Math.floor(Math.random() * (max + 1 - min)) + min);
-          setAfter(Math.floor(Math.random() * (nmax + 1 - nmin)) + nmin);
-          setBefore(Math.floor(Math.random() * (nmax + 1 - nmin)) + nmin);
           setVisible(false);
         }}
       >
@@ -81,10 +90,6 @@ function App() {
       <p style={{ margin: 10 }}>
         {" "}
         ＊ 2〜16進表示の中で、ランダムに問題が作成されます。
-      </p>
-      <p style={{ margin: 10 }}>
-        {" "}
-        ＊ 時々、「n進からn進に直しましょう」というしょーもない問題が出てきますが悪しからず。
       </p>
     </>
   );
