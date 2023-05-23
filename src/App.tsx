9ea4a652ce94974560c8de81c769bef69d572110
random776 <kazuki.tdn@gmail.com>
2023-05-23 22:48:33 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,31 @@ function App() {
   const [after, setAfter] = useState(b);
   // 見えるか見えないか
   const [visible, setVisible] = useState(false);
+  const [hidden, setHidden] = useState(false);
+  //公式の作成（前半）
+  const numberArrayPrimer = [...number.toString(before)];
+  const numberArray = numberArrayPrimer.map((value) => ( //16進数表記を10進数表記に訂正
+    parseInt(value, 16)
+  ));
+  const popedNumber = numberArray.pop();
+  const formula = numberArray.map((value, i) => (
+    <MathComponent
+          display={false}
+          tex={String.raw` ${value} \times ${before}^{${numberArray.length - (i)}} +\,`}
+        ></MathComponent>
+  ));
+  //公式の作成（後半）
+  const numberArrayPrimer2 = [...number.toString(after)];
+  const numberArray2 = numberArrayPrimer2.map((value) => ( //16進数表記を10進数表記に訂正
+    parseInt(value, 16)
+  ));
+  const popedNumber2 = numberArray2.pop();
+  const formula2 = numberArray2.map((value, i) => (
+    <MathComponent
+          display={false}
+          tex={String.raw` ${value} \times ${after}^{${numberArray2.length - (i)}} +\,`}
+        ></MathComponent>
+  ));
   return (
     <>
       <h2>フラッシュn進法ニキ</h2>
@@ -58,9 +83,75 @@ function App() {
           tex={String.raw`${number.toString(after)}_{(${after})}`}
         ></MathComponent>
       </p>
+      <p>
+      {hidden === false && (
+        <button
+          type="button"
+          key={"answer2"}
+          onClick={() => {
+            setHidden(true);
+          }}
+        >
+          解説を表示
+        </button>
+      )}
+      {hidden === true && (
+        <button
+          type="button"
+          key={"answer2"}
+          onClick={() => {
+            setHidden(false);
+          }}
+        >
+          解説を非表示
+        </button>
+      )}
+      </p>
+      <p style={{ display: hidden ? "block" : "none" }}>
+      <div className={"kaisetsu"}>
+        <p>
+        与えられた数を10進法で表すと、
+        </p>
+      <MathComponent
+          display={false}
+          tex={String.raw`${number.toString(before)}_{(${before})} =\,`}
+        ></MathComponent>
+        {formula}
+        <MathComponent
+          display={false}
+          tex={String.raw`${popedNumber} \times ${before}^${0} =${number}`}
+        ></MathComponent>
+        <p>
+          と計算できる。
+        </p>
+        <p>
+        また、<MathComponent
+          display={false}
+          tex={String.raw`${after}^${numberArray2.length}<${number}<${after}^${numberArray2.length + 1} \:`}
+        ></MathComponent>であるから、ここから{after}進法に直すと、
+        </p>
+      <MathComponent
+          display={false}
+          tex={String.raw`${number}=\,`}
+        ></MathComponent>
+        {formula2}
+      <MathComponent
+          display={false}
+          tex={String.raw`${popedNumber2} \times ${after}^${0} =\,`}
+        ></MathComponent>
+        <MathComponent
+          display={false}
+          tex={String.raw`\, ${number.toString(after)}_{(${after})}`}
+        ></MathComponent>
+        <p>
+          と求まり、答えを得る。
+        </p>
+      </div>
+      </p>
       <button
         type="button"
         key={"button"}
+        className={"kirikae"}
         onClick={() => {
           let a = Math.floor(Math.random() * (nmax + 1 - nmin)) + nmin;
           let b = Math.floor(Math.random() * (nmax + 1 - nmin)) + nmin;
@@ -72,6 +163,8 @@ function App() {
           setBefore(b);
           setNumber(Math.floor(Math.random() * (max + 1 - min)) + min);
           setVisible(false);
+          setHidden(false);
+          console.log(formula);
         }}
       >
         整数を切り替える
